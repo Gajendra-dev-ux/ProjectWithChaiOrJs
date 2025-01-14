c1082004daeb6e9ab5a4a9f5d115dc20156d0a05
Gajendra <gajendra.14051@gmail.com>
2025-01-14 21:10:14 +0530

    Added 2 more projects

---
+++ unlimitedColors/script.js
@@ -0,0 +1,25 @@
+//generate a random color
+
+const randomColor = function(){
+    const hex = '0123456789ABCDEF'
+    let color = '#'
+    for(let i=0;i<6;i++){
+        color += hex[Math.floor(Math.random()*16)];
+    }
+    return color;
+};
+
+let stopColorChange;
+
+document.querySelector('#start').addEventListener('click',function(){
+    if(!stopColorChange){
+        stopColorChange = setInterval(function(){
+            document.body.style.backgroundColor = randomColor()
+        },1000)
+    }
+})
+
+document.querySelector('#stop').addEventListener('click',function(){
+    clearInterval(stopColorChange)
+    stopColorChange = null
+})
